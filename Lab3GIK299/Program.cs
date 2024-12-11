7c2b4e4cd4be6afb62ac59331dc1bc0ea906fa72
Viktor <viktoreriksson@live.se>
2024-12-11 09:58:14 +0100

    added comments

--- Lab3GIK299/Program.cs
+++ Lab3GIK299/Program.cs
@@ -1,15 +1,17 @@
-﻿using System.Threading.Channels;
-
-namespace Lab3GIK299;
+﻿namespace Lab3GIK299;
 
 class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
+        //initialise an object 
         TemperatureCalculator temperatureCalculator = new TemperatureCalculator();
+        
+        //while loop to keep the program running until user want to exit
         bool keepRunning = true;
         while (keepRunning)
         {
+            //present the user with a menu
             Console.WriteLine(@"+------------------------------------------------------------------------------------------+
 |            WELCOME TO THE TEMPERATURECALCULATOR v1.0 (https://t.ly/vDr_z)                |
 |						      by Viktor and Elvira                 |
@@ -30,7 +32,8 @@ class Program
 +------------------------------------------------------------------------------------------+");
             string userMenuInput = Console.ReadKey().KeyChar.ToString().ToLower();
             Console.WriteLine();
-
+            
+            //takes the user choice and calls for the function.
             switch (userMenuInput)
             {
                 case "1":
@@ -49,9 +52,11 @@ class Program
                     temperatureCalculator.DisplayMedianTemperature();
                     break;
                 case "6":
+                    //keep prompting the user for correct input
                     bool keepRunning6 = true;
                     while (keepRunning6)
                     {
+                        //prompts the user for sorting preference
                         Console.WriteLine("Do you want to sort its temperatures in 1.ascending or 2.descending order?");
                         string userInput = Console.ReadKey().KeyChar.ToString();
                         if (userInput is "1" or "2")
@@ -63,11 +68,13 @@ class Program
                     }
                     break;
                 case "7":
+                    //prompts user for a choice before calling function
                     Console.WriteLine("What threshold do you want to set?");
                     double userThresholdInput = double.Parse(Console.ReadLine() ?? string.Empty);
                     temperatureCalculator.FilterTemperatureThreshold(userThresholdInput);
                     break;
                 case "8":
+                    //prompts user for choice before calling function
                     Console.WriteLine("What day of the month do you want to see? Enter a number.");
                     int userDayNumberInput = Convert.ToInt32(Console.ReadLine());
                     temperatureCalculator.FilterTempreratureBySearch(userDayNumberInput);
@@ -86,7 +93,7 @@ class Program
                     
                     
             }
-
+            //ask if the user wants to quit and end the while loop
             Console.WriteLine();
             Console.WriteLine("Do you want to exit the program? (Y/N) ");
             string userInputContinue = Console.ReadKey().KeyChar.ToString().ToUpper();
@@ -99,10 +106,8 @@ class Program
         }
         
         
-        
-        
     }
-
+    //dont worry about it 
     static void SuperSecretFunction()
     {
         string url = "https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley";

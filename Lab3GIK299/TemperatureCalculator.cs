8c40f2a80f6acec0ddbb306c1106857b6ea47e58
Viktor <viktoreriksson@live.se>
2024-12-11 10:15:44 +0100

    added comments TC

--- Lab3GIK299/TemperatureCalculator.cs
+++ Lab3GIK299/TemperatureCalculator.cs
@@ -2,10 +2,13 @@ namespace Lab3GIK299;
 
 public class TemperatureCalculator
 {
-    
+    // A Random instance to generate random temperatures.
     private readonly Random _random = new Random();   
+
+    // Array to hold temperatures for 31 days.
     private readonly double[] _temperatures; 
     
+    // Constructor initializes the array with random temperatures between 8.0 and 20.0.
     public TemperatureCalculator()
     {
         _temperatures = new double[31];
@@ -15,6 +18,7 @@ public class TemperatureCalculator
         }
     }
 
+    // Method to display temperatures for each day of May in a table format.
     public void DisplayTempratureForEachDay()
     {
         Console.WriteLine("The temperature each day in May was:");
@@ -25,10 +29,10 @@ public class TemperatureCalculator
         {
             Console.WriteLine($"|May {i +1,2}  | {_temperatures[i],5:F1} \u00B0C    |");
             Console.WriteLine("+----------------------+");
-            //Console.WriteLine($"The temprature for May {i +1} was {_temperatures[i]:F1} \u00B0C");
         }
     }
 
+    // Method to calculate and display the average temperature of May.
     public void FindAverageTemperature()
     {
         double average = _temperatures.Average();
@@ -37,47 +41,54 @@ public class TemperatureCalculator
         Console.WriteLine("+------------------------------------------+");
     }
 
+    // Method to find and display the highest temperature of May.
     public void FindMaxTemperature()
     {
         double max = _temperatures.Max();
         int maxIndex = Array.IndexOf(_temperatures, max);
-        Console.WriteLine($"The highest temperature in May was: ");
-        Console.WriteLine($"May {maxIndex} and the temperature was {max:F1} \u00B0C");
+        Console.WriteLine("The highest temperature in May was: ");
+        Console.WriteLine($"May {maxIndex + 1} and the temperature was {max:F1} \u00B0C");
     }
 
+    // Method to find and display the lowest temperature of May.
     public void FindMinTemperature()
     {
         double min = _temperatures.Min();
         int minIndex = Array.IndexOf(_temperatures, min);
-        Console.WriteLine($"The lowest temperature in May was: ");
-        Console.WriteLine($"May {minIndex} and the temperature was {min:F1} \u00B0C");
+        Console.WriteLine("The lowest temperature in May was: ");
+        Console.WriteLine($"May {minIndex + 1} and the temperature was {min:F1} \u00B0C");
     }
 
+    // Method to display the median temperature of May.
     public void DisplayMedianTemperature()
     {
         int length = _temperatures.Length;
         if (length % 2 != 0)
         {
+            // Odd number of elements: median is the middle value.
             int medianIndex = length / 2;
             Console.WriteLine($"The median temperature in May was {_temperatures[medianIndex]:F1} \u00B0C");
         }
         else
         {
+            // Even number of elements: median is the average of the two middle values.
             int medianIndex1 = length / 2 - 1;
             int medianIndex2 = length / 2;
-            int averageMedian = Convert.ToInt32(_temperatures[medianIndex1] + _temperatures[medianIndex2] / 2);
-            Console.WriteLine($"The median temperature in May was {_temperatures[averageMedian]:F1} \u00B0C");
+            double averageMedian = (_temperatures[medianIndex1] + _temperatures[medianIndex2]) / 2;
+            Console.WriteLine($"The median temperature in May was {averageMedian:F1} \u00B0C");
         }
     }
 
+    // Method to sort and display the temperatures in ascending or descending order.
     public void SortTemperature(string arg)
     {
         double[] copyArray = new double[_temperatures.Length];
         Array.Copy(_temperatures, copyArray, _temperatures.Length);
         Array.Sort(copyArray);
+
         if (arg == "1")
         {
-            Console.WriteLine("The the tempratures in May in ascending order is:");
+            Console.WriteLine("The temperatures in May in ascending order are:");
             for (int i = 0; i < copyArray.Length; i++)
             {
                 Console.WriteLine($"{copyArray[i]:F1} \u00B0C");
@@ -86,7 +97,7 @@ public class TemperatureCalculator
         else
         {
             Array.Reverse(copyArray);
-            Console.WriteLine("The the tempratures in May in decending order is:");
+            Console.WriteLine("The temperatures in May in descending order are:");
             for (int i = 0; i < copyArray.Length; i++)
             {
                 Console.WriteLine($"{copyArray[i]:F1} \u00B0C");
@@ -94,23 +105,32 @@ public class TemperatureCalculator
         }
     }
 
+    // Method to filter and display days with temperatures above a certain threshold.
     public void FilterTemperatureThreshold(double arg)
     {
-        Console.WriteLine($"Day´s where the temperature in May was above {arg} was: ");
+        Console.WriteLine($"Days where the temperature in May was above {arg} were: ");
         for (int i = 0; i < _temperatures.Length; i++)
         {
-            if (arg <= _temperatures[i])
+            if (_temperatures[i] > arg)
             {
-                Console.WriteLine($"May {i +1} with an temperature of {_temperatures[i]:F1} \u00B0C");
+                Console.WriteLine($"May {i + 1} with a temperature of {_temperatures[i]:F1} \u00B0C");
             }
         }
     }
 
+    // Method to search and display the temperature of a specific day and adjacent days.
     public void FilterTempreratureBySearch(int arg)
     {
-        Console.WriteLine($"The temperature May {arg} was {_temperatures[arg -1]:F1}");
-        Console.WriteLine($"The day before the temperature was {_temperatures[arg -2]:F1} \u00B0C");
-        Console.WriteLine($"The day after the temperature was {_temperatures[arg]:F1} \u00B0C");
+        Console.WriteLine($"The temperature on May {arg} was {_temperatures[arg - 1]:F1} \u00B0C");
+        if (arg > 1)
+        {
+            Console.WriteLine($"The day before, the temperature was {_temperatures[arg - 2]:F1} \u00B0C");
+        }
+
+        if (arg < _temperatures.Length)
+        {
+            Console.WriteLine($"The day after, the temperature was {_temperatures[arg]:F1} \u00B0C");
+        }
     }
 
     public void FindMostCommonTemperature()
@@ -127,3 +147,6 @@ public class TemperatureCalculator
         Console.WriteLine($"The most common temperature in May was {mode:F1} \u00B0C");
     }
 }
+
+    
+
